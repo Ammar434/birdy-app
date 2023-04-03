99577e22897f7ef360ffd97433e35ff501d9423c
cha_am <aya.belarbi16@gmail.com>
2023-04-03 14:25:39 +0200

    structure home

---
+++ archive/MainPage.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./MainPage.css";
+const Home = () => {
+  
+  return (
+    <div>
+      MainPage
+      <header>
+        <div id="logo">LOGO</div>
+        <div id="search">BARRE DE RECHERCHE</div>
+        <div id="connect">
+          <a href="C:\Users\zhaam\Desktop\TechnoWeb\TME2\index.html">
+            CONNECTEZ-VOUS
+          </a>
+        </div>
+      </header>
+      <main>
+        <aside> ZONE STATISTIQUE</aside>
+        <section>
+          <div id="commentaire">ZONE NOUVEAU COMMENTAIRE</div>
+
+          <div id="liste_commentaire">
+            LISTE DE COMMENTAIRE
+            <ul>
+              <li>Commentaire 1 </li>
+              <li>Commentaire 2 </li>
+              <li>Commentaire 3 </li>
+              <li>Commentaire 4 </li>
+            </ul>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/pages/home_pages/LeftSideBar/MenuNavigation.js
@@ -0,0 +1,17 @@
+//create a componennt MenuNaviagation
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Box, Flex, Text, Image, Button } from '@chakra-ui/react';
+import { HamburgerIcon } from '@chakra-ui/icons';
+import { useMediaQuery } from '@chakra-ui/media-query';
+import { useColorModeValue } from '@chakra-ui/color-mode';
+import { useDisclosure } from '@chakra-ui/hooks';
+import { useBreakpointValue } from '@chakra-ui/media-query';
+
+import { MenuDrawer } from './MenuDrawer';
+
+const MenuNavigation = () => {
+
+}
+
+export default MenuNavigation; 
